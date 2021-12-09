b075e28e000f62cf475920c66c6d59f9c627aa24
Susej Reina <susejreina@gmail.com>
2021-12-09 09:54:54 -0300

    Fisrt commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/*
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+*/
+const Li = ({ children, estado }) => 
+  <li>{children} estoy {estado}</li>
+
+const X = () => 
+  <ul>
+    <Li estado={'feliz'}>Chanchito </Li>
+    <li>Chanchito triste</li>
+    <li>Felipe</li>
+  </ul>
+ReactDOM.render(
+ <X /> , document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
